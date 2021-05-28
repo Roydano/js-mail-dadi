11544bb3b6e4bb60d5eacad2bca5ff13abc2a2fa
Roydano <roy.dano90@gmail.com>
2021-05-28 15:57:08 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Gioco dei dadi Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// 1 - chiedere al giocatore di inserire il suo nome
+
+// generare un numero random per il giocatore
+
+// generare un numero random per il pc
+
+// dichiarare chi ha vinto in base al punteggio più alto
